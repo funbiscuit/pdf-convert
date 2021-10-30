0a380d3e956baaca06e9e587d33ac3653804b817
funbiscuit <kokurinsv@gmail.com>
2021-10-30 14:20:57 +0300

    Update native image configuration
    Add jbig2 and j2k support

--- src/main/java/com/funbiscuit/pdfconvert/PdfMain.java
+++ src/main/java/com/funbiscuit/pdfconvert/PdfMain.java
@@ -1,12 +1,19 @@
 package com.funbiscuit.pdfconvert;
 
+import com.github.jaiimageio.jpeg2000.impl.J2KImageReaderSpi;
 import lombok.SneakyThrows;
+import org.apache.pdfbox.jbig2.JBIG2ImageReaderSpi;
 import picocli.CommandLine;
+
+import javax.imageio.spi.IIORegistry;
 
 
 public class PdfMain {
     @SneakyThrows
     public static void main(String[] args) {
+        IIORegistry.getDefaultInstance().registerServiceProvider(new JBIG2ImageReaderSpi());
+        IIORegistry.getDefaultInstance().registerServiceProvider(new J2KImageReaderSpi());
+
         int exitCode = new CommandLine(new PdfConvertCommand()).execute(args);
         System.exit(exitCode);
     }
